81cfa9e4cad71edf1d84b898c0c69e98ff58c998
valerian <you@example.com>
2021-01-15 17:06:22 +0100

    create hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 const Hero = ({ children, hero }) => {
+  
   return <header className={hero}>
-    {children}
+   {children}
   </header>;
 };
+
+Hero.defaultProps = {
+  hero:"defaultHero"
+}
 
 export default Hero;
