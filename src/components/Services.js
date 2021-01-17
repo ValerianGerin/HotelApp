4775ceb770939bb8a3b57965ca581860d0aba124
valerian <you@example.com>
2021-01-17 12:13:13 +0100

    service component finished

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,13 +3,45 @@ import Title from "./Title";
 import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
 
 const Services = () => {
-  const [services, setServices] = useState({
-    services: [{ icon: <FaCocktail />, title: "free cocktails" }],
-  });
+  const [services, setServices] = useState([
+    {
+      icon: <FaCocktail />,
+      title: "free cocktails",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing alit. Magni, corporis!",
+    },
+    {
+      icon: <FaHiking />,
+      title: "Endless Hiking",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing alit. Magni, corporis!",
+    },
+    {
+      icon: <FaShuttleVan />,
+      title: "Free shuttle",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing alit. Magni, corporis!",
+    },
+    {
+      icon: <FaBeer />,
+      title: "Strongest beer",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing alit. Magni, corporis!",
+    },
+  ]);
   return (
-    <div>
+    <section className="services">
       <Title title="services" />
-    </div>
+      <div className="services-center">
+        {services.map((item, i) => (
+          <article key={i} className="service">
+            <span>{item.icon}</span>
+            <h6>{item.title}</h6>
+            <p>{item.info}</p>
+          </article>
+        ))}
+      </div>
+    </section>
   );
 };
 
