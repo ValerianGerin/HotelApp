6e2c859da9caeebb2fe01db4c953c8ae1d270786
valerian <you@example.com>
2021-01-17 11:50:32 +0100

    adding banner and link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,24 @@
 import React from "react";
 import Hero from "./Hero";
 import Banner from "./Banner";
+import Services from "./Services";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <Hero>
-      <Banner title="luxurious rooms" subtitle="deluxe rooms strating at 299€">
-        <Link to="/rooms" className="btn-primary">our rooms</Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero>
+        <Banner
+          title="luxurious rooms"
+          subtitle="deluxe rooms strating at 299€"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </>
   );
 };
 
