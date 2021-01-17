b01a03a69cbcdd679d2b12a205634d0737e28183
valerian <you@example.com>
2021-01-17 11:50:05 +0100

    adding banner and link

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
 import Hero from "./Hero";
+import Banner from "./Banner";
+import { Link } from "react-router-dom";
+
+
 const Rooms = () => {
   return (
-    <Hero hero="roomsHero"/>
-  )
-}
+    <Hero hero="roomsHero">
+      <Banner title="our rooms">
+        <Link to="/" className="btn-primary">
+          return home
+        </Link>
+      </Banner>
+    </Hero>
+  );
+};
 
-export default Rooms
+export default Rooms;
