b645da0389ad83ebfa265b223fad1021afe57dbd
valerian <you@example.com>
2021-01-17 11:49:27 +0100

    create Service and Title

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
+
+const Title = ({ title }) => {
+  return (
+    <div className="section-title">
+      <h4>{title}</h4>
+      <div />
+    </div>
+  );
+};
+
+export default Title;
