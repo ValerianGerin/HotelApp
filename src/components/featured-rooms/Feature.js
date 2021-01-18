48fa4d179cbd01b3b69232b8c972ef72af950581
valerian <you@example.com>
2021-01-18 11:39:20 +0100

    delete test in component

--- src/components/featured-rooms/Feature.js
+++ src/components/featured-rooms/Feature.js
@@ -1,10 +1,10 @@
-import React, { Component, useContext } from "react";
+import React, { useContext } from "react";
 import { RoomContext } from "../../Context";
 
 const Feature = () => {
-  const context = useContext(RoomContext);
-  console.log(context);
-  return <div>Hello from feature</div>;
+
+ 
+  return <div>Hello from feature </div>;
 };
 
 export default Feature;
