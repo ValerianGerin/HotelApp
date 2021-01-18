720e4aa7cc458dcadf13d9e186c108d845d675c7
valerian <you@example.com>
2021-01-18 11:38:24 +0100

    setting state in context

--- src/Context.js
+++ src/Context.js
@@ -1,17 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import data from "./data";
 
 const RoomContext = React.createContext();
 
-const RoomProvider = (props)=> {
-  const [contextRoom, setContextRomm] = useState("toto")
- 
-    return (
-      <RoomContext.Provider value={contextRoom}>
-        {props.children}
-      </RoomContext.Provider>
-    );
-  
-}
+const RoomProvider = (props) => {
+  const [context, setContextRoom] = useState({
+    rooms: [],
+    sortedRooms: [],
+    featured: [],
+    loading: true,
+  });
+
+  //Function to pretend fetching the data from external source with a delay
+  useEffect(() => {
+    const fetching = setTimeout(() => {
+      setContextRoom({...context, rooms: data, loading: false})
+    }, 2000);
+    return () => {
+      clearInterval(fetching)
+      
+    }
+  }, [])
+
+  console.log(context)
+
+
+
+  return (
+    <RoomContext.Provider value={{ ...context }}>
+      {props.children}
+    </RoomContext.Provider>
+  );
+};
 
 const RoomConsumer = RoomContext.Consumer;
 
