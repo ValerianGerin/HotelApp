6dba4b12fc694512b6c1f15377db004f55e6cacc
valerian <you@example.com>
2021-01-18 12:53:32 +0100

    ternary to etablish if render loader or featuredRooms

--- src/components/featured-rooms/Feature.js
+++ src/components/featured-rooms/Feature.js
@@ -1,10 +1,22 @@
 import React, { useContext } from "react";
 import { RoomContext } from "../../Context";
+import { Loading, Room, Title } from "../index";
 
 const Feature = () => {
+  const { loading, featuredRooms } = useContext(RoomContext);
 
- 
-  return <div>Hello from feature </div>;
+  return (
+    <section className="featured-rooms">
+      <Title title="featured-rooms" />
+      <div className="featured-rooms-center">
+        {loading ? (
+          <Loading />
+        ) : (
+          featuredRooms.map((room) => <Room key={room.id} room={room} />)
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Feature;
